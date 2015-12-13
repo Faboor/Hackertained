768a9ed764c6ba181fe81f7e000718299ac192df
Peter Kovary <peter.kovary15@imperial.ac.uk>
2015-12-13 10:29:11 +0000

    Added counter

---
+++ src/Counter.java
@@ -0,0 +1,27 @@
+/**
+ * Created by Acer on 13-Dec-15.
+ */
+public class Counter {
+
+  private int ticks;
+
+  public Counter() {
+    ticks = 0;
+  }
+
+  public Counter(int ticks) {
+    this.ticks = ticks;
+  }
+
+  public void tick() {
+    ticks++;
+  }
+
+  public int getTicks() {
+    return ticks;
+  }
+
+  @Override public String toString() {
+    return String.valueOf(ticks);
+  }
+}
